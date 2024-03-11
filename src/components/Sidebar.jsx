a3f0e59d17e4c0783ac1c7ec97937590e7628db3
Ezechiel Mukenge <127405428+Ezmuk@users.noreply.github.com>
2024-03-11 10:25:11 +0200

    le  composant Sidebar et ses sous composants
    le  composant Sidebar et ses sous composants

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,29 @@
-export default function Sidebar(){
+import Button from "./Button"
+import TweetAction from "./TweetAction"
+import TweetWhite from "../assets/tweetWhite.svg"
+import Home_Fill from "../assets/Home-Fill.svg"
+import Explore_icn from "../assets/Explore.svg"
+import Notificpic from "../assets/Notificpic.svg"
+import Message_icn from "../assets/Messages.svg"
+import Bookmarks_icn from "../assets/Bookmarks.svg"
+import Lists_icn from "../assets/Lists.svg"
+import Profile_icn from "../assets/Profile.svg"
+import More_icn from "../assets/More.svg"
+export default function Sidebar(props){
     return(
         
-        <div>Siderbar</div>
+        <div className="sidebar">
+            <TweetAction  img={TweetWhite} />
+            <TweetAction  img={Home_Fill} value="Home"/>
+            <TweetAction  img={Explore_icn} value="Explore"/>
+            <TweetAction  img={Notificpic} value="Notifications"/>
+            <TweetAction  img={Message_icn} value="Messages"/>
+            <TweetAction  img={Bookmarks_icn} value="Bookmarks"/>
+            <TweetAction  img={Lists_icn} value="Lists"/>
+            <TweetAction  img={Profile_icn} value="Profile"/>
+            <TweetAction  img={More_icn} value="more"/>
+            <Button value="Tweet"/>
+        </div>
         
     )
 }
