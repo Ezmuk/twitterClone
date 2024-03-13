ffe4b3b24274af948188b6ce3cca0ceecf882555
Ezechiel Mukenge <127405428+Ezmuk@users.noreply.github.com>
2024-03-13 09:32:37 +0200

    Elever la repetion d'un composant
    To use the map() function in the given code, you can replace the repeating TrendingIn components with a map() function that will loop through an array of objects and render a TrendingIn component for each object

--- src/components/TrendsForYou.jsx
+++ src/components/TrendsForYou.jsx
@@ -3,14 +3,15 @@ import TrendsFuTitle from "./TrendsFuTitle"
 import Dot_icn from "../assets/More-2.svg"
 import TrendingIn from "./TrendingIn"
 import ShowMore from "./ShowMore"
+
 export default function TrendsForYou(props){
+    const trendingData = Array.from({length: 4}, (_, i) => i + 1); // Replace this with actual data
     return(
         <div className="trends_f_u">
             <TrendsFuTitle img={Setting} value="Trends For you"/>
-            <TrendingIn icn={Dot_icn}/>
-            <TrendingIn icn={Dot_icn}/>
-            <TrendingIn icn={Dot_icn}/>
-            <TrendingIn icn={Dot_icn}/>
+            {trendingData.map((item, index) => (
+              <TrendingIn key={index} icn={Dot_icn}/>
+            ))}
             <ShowMore/>
         </div>
     )
