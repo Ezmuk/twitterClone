b03b036268c2a925537c3a253c4a169bd9c9bffd
Ezechiel Mukenge <127405428+Ezmuk@users.noreply.github.com>
2024-04-15 10:48:25 +0200

    Affichage des tweets depuis ‘API (le fichier json)
    Affichage des tweets depuis ‘API (le fichier json)

--- src/Provider/TweetProvider.jsx
+++ src/Provider/TweetProvider.jsx
@@ -1,10 +1,23 @@
 import React, { useState } from "react";
 import { TweetContext } from "../context/context";
-import data from "../Data/initial-data.json";
+import axios from "axios";
 
 export default function TweetProvider({ children }) {
-  const donne = data.tweets;
-  const [post, setPost] = useState(donne);
+  const [post, setPost] = useState([]);
+  const [error, setError] = useState(null);
+
+  axios
+    .get("http://localhost:3000/tweets")
+    .then((response) => {
+      setPost(response.data);
+    })
+    .catch((error) => {
+      setError(error);
+    });
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
 
   return (
     <TweetContext.Provider value={{ post, setPost }}>
