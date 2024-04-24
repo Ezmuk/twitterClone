556d47fd2a242e1396ec8ded3137868324256aab
Ezechiel Mukenge <127405428+Ezmuk@users.noreply.github.com>
2024-04-24 08:24:33 +0200

    requiered égale à true
    requiered égale à true

--- src/components/TweetEditorInput.jsx
+++ src/components/TweetEditorInput.jsx
@@ -5,7 +5,7 @@ export default function TweetEditorInput({ register }) {
         className="tweet-editor-input w-full h-[60px] border-0 outline-none text-xl bg-black text-white resize-none pt-4 pb-0 px-0 my-3 md:my-5"
         placeholder="What's happening ?"
         {...register("tweet")}
-        required
+        required="true"
       />
     </div>
   );
